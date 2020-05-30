cb26dfa49da37412d2fff979d241500083d0feaa
mikasa <onlymisaky@gmail.com>
2020-05-30 19:25:50 +0800

    feat: GeedStorage & README

---
+++ README.md
@@ -0,0 +1,38 @@
+# GeedStorage
+
+支持 `josn` 的本地存储。
+
+## Features
+- 支持存储和读出 `string`、`numer`、`object`、`boolean`、`undefined`.
+- 支持设置存储key的前辍
+- 支持设置过期时间
+- 友好的类型提示
+
+## API
+```typescript
+const storage = new GeedStorage({
+  type: 'local',    // local | session
+  prefix: 'Geed_',  // 默人为 Geed_
+});
+
+/** set */
+storage.set('name', 'qq');
+storage.set('age', 18);
+storage.set('busy', true);
+storage.set('user', { name: 'qq', age: 18 }, { expires: 1000 * 60 }); // 1分钟后再次读取将会自动清空并返回 null
+
+/** get */
+storage.get('age'); // 'qq' string
+storage.get('name'); // 18 numer
+storage.get('busy'); // true boolean
+storage.get<{
+  name: string;
+  age: number
+}>('user'); // { name: 'qq', age: 18 }
+
+/** remove */
+
+storage.remove('busy'); // remove busy
+
+storage.clearAll(); // clear all keys
+```
---
+++ src/index.ts
@@ -0,0 +1,104 @@
+type Extra = {
+  type: 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function';
+  expires: number | 'long';
+}
+
+class StorageValue<T> {
+  value!: T;
+  type!: 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function';
+  expires: number | 'long';
+  update!: number;
+
+  constructor(value: T, extra: Extra) {
+    this.value = value;
+    this.type = extra.type;
+    this.expires = extra.expires;
+    this.update = Date.now();
+  }
+}
+
+export class GeedStorage {
+
+  private type!: 'session' | 'local';
+  private prefix = 'Geed_';
+  private storage!: Storage;
+
+  constructor(options: { type?: 'session' | 'local'; prefix?: string; } = { type: 'session', prefix: 'Geed_' }) {
+    this.type = options.type || 'session';
+    this.prefix = options.prefix || 'Geed_';
+    this.storage = this.type === 'session' ? sessionStorage : localStorage;
+  }
+
+  set<T>(key: string, value: T, options?: { expires?: number; }) {
+    const val = this.serialize(value, options?.expires as number);
+    return this.storage.setItem(`${this.prefix}${key}`, val);
+  }
+  get<T>(key: string): T | null {
+    let storageValue;
+    storageValue = this.storage.getItem(`${this.prefix}${key}`);
+    if (storageValue) {
+      storageValue = JSON.parse(storageValue) as StorageValue<T>;
+      let { expires, type, value, update } = storageValue;
+      const isExpired = this.isExpired(update, expires);
+      if (isExpired) {
+        this.remove(key);
+        return null;
+      }
+      switch (type) {
+        case 'number':
+          value = Number(value) as unknown as T;
+          break;
+        case 'undefined':
+          value = undefined as unknown as T;
+          break;
+        case 'boolean':
+          if (value as unknown as string === 'false') {
+            value = false as unknown as T;
+          } else if (value as unknown as string === 'true') {
+            value = true as unknown as T;
+          } else {
+            value = Boolean(value) as unknown as T;
+          }
+          break;
+        case 'string':
+          value = `${value}` as unknown as T;
+          break;
+        default:
+          break;
+      }
+      return value;
+    }
+    return null;
+  }
+  remove(key: string) {
+    return this.storage.removeItem(`${this.prefix}${key}`)
+  }
+  clearAll() {
+    return this.storage.clear();
+  }
+
+  private serialize(value: any, expires: number | 'long'): string {
+    const type = typeof value;
+    let val: any;
+    if (['bigint', 'symbol', 'function'].includes(type)) {
+      val = new StorageValue('', { type, expires })
+    }
+    else {
+      val = new StorageValue(value, { type, expires })
+    }
+    return JSON.stringify(val);
+  }
+
+  private isExpired(update: number, expires: any): boolean {
+    const expireDate = parseInt(expires, 10);
+    if (isNaN(expireDate)) {
+      return false;
+    }
+    const now = Date.now();
+    if (now > update + expireDate) {
+      return true;
+    }
+    return false;
+  }
+
+}
